04264617fe46b42765bf2a0ddc3f9de242c9ab75
Alon Kiriati <alonk@dropbox.com>
2020-07-11 18:16:56 +0300

    more client side logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import Watermark from './watermark';
+import NonWatermarkedSection from './nonWatermarkedSection';
 import './App.css';
 
 class App extends React.Component {
-  
-  constructor(props){
+
+  constructor(props) {
     super(props)
     this.state = {
-      watermark: null
+      watermark: null,
+      nonWatermarkedIds: [],
+      watermarkedIds: []
     }
 
   }
 
   handleWatermarkChanged = (event) => {
-    //const formData = new FormData()
-    //formData.append('watermark',event.target.files[0]);
     fetch("http://localhost:3000/files/watermark.png", {
       method: 'POST',
-      body: event.target.files[0], 
+      body: event.target.files[0],
       headers: {
         'content-type': 'image/png'
-      }   
+      }
     }).then(
       this.setState({
         watermark: window.URL.createObjectURL(event.target.files[0])
@@ -28,23 +29,46 @@ class App extends React.Component {
     )
   }
 
+  handleUploadNonWatermarkedPictures = (event) => {
+    for (let file in event.target.files) {
+      fetch("http://localhost:3000/actions/uploadNonWatermarkedPictures", {
+        method: 'POST',
+        body: file,
+        headers: {
+          'content-type': 'image/png'
+        }
+      }).then(response => {
+        this.setState({
+          nonWatermarkedIds: this.state.nonWatermarkedIds.append(response)
+        })
+      })
+    }
+  }
+
+
+
+
   componentDidMount = () => {
     fetch("http://localhost:3000/files/watermark.png")
-    .then(response => response.blob())
-    .then(blob =>  
-      this.setState({
-        watermark: window.URL.createObjectURL(new File([blob], "name"))
-      }))
-    }
+      .then(response => response.blob())
+      .then(blob =>
+        this.setState({
+          watermark: window.URL.createObjectURL(new File([blob], "name"))
+        }))
+  }
 
 
   render() {
     return (
       <div>
-        <Watermark 
-        {...this.state} 
-        handleWatermarkChanged={this.handleWatermarkChanged}
+        <Watermark
+          {...this.state}
+          handleWatermarkChanged={this.handleWatermarkChanged}
         ></Watermark>
+        <NonWatermarkedSection
+          {...this.state}
+        >
+        </NonWatermarkedSection>
       </div>
     );
   }
