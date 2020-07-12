ce6d6c172103e1a1c292e1413d956a530c5689d2
Alon Kiriati <alonk@dropbox.com>
2020-07-12 11:09:16 +0300

    more...

--- src/App.js
+++ src/App.js
@@ -31,49 +31,62 @@ class App extends React.Component {
 
   handleUploadNonWatermarkedPictures = (event) => {
     for (let file in event.target.files) {
-      fetch("http://localhost:3000/actions/uploadNonWatermarkedPictures", {
+      let picId = this.uuidv4()
+      fetch("http://localhost:3000/files/" + picId + ".png", {
         method: 'POST',
         body: file,
         headers: {
           'content-type': 'image/png'
         }
       }).then(response => {
-        this.setState({
-          nonWatermarkedIds: this.state.nonWatermarkedIds.append(response)
-        })
+        fetch("http://localhost:3000/addNewPicId", {
+          method: 'POST',
+          body: { picId: picId },
+        }).then(response =>
+          this.setState({
+            nonWatermarkedIds: this.state.nonWatermarkedIds.append(picId)
+          })
+        )
       })
+    }
+  }
+
+  uuidv4 = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+
+
+
+
+    componentDidMount = () => {
+      fetch("http://localhost:3000/files/watermark.png")
+        .then(response => response.blob())
+        .then(blob =>
+          this.setState({
+            watermark: window.URL.createObjectURL(new File([blob], "name"))
+          }))
+    }
+
+
+    render() {
+      return (
+        <div>
+          <Watermark
+            {...this.state}
+            handleWatermarkChanged={this.handleWatermarkChanged}
+          ></Watermark>
+          <NonWatermarkedSection
+            {...this.state}
+          >
+          </NonWatermarkedSection>
+        </div>
+      );
     }
   }
 
 
 
-
-  componentDidMount = () => {
-    fetch("http://localhost:3000/files/watermark.png")
-      .then(response => response.blob())
-      .then(blob =>
-        this.setState({
-          watermark: window.URL.createObjectURL(new File([blob], "name"))
-        }))
-  }
-
-
-  render() {
-    return (
-      <div>
-        <Watermark
-          {...this.state}
-          handleWatermarkChanged={this.handleWatermarkChanged}
-        ></Watermark>
-        <NonWatermarkedSection
-          {...this.state}
-        >
-        </NonWatermarkedSection>
-      </div>
-    );
-  }
-}
-
-
-
-export default App;
+  export default App;
