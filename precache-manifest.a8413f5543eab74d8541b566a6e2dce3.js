b885f5ca7730f522d71f8c3e7be895985d4ce7af
Alon Kiriati <alonk@dropbox.com>
2020-07-15 18:03:17 +0300

    Updates

--- precache-manifest.a8413f5543eab74d8541b566a6e2dce3.js
+++ precache-manifest.a8413f5543eab74d8541b566a6e2dce3.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9726f628dc0a5ebdb3723afa52677f68",
+    "revision": "99431f05d6bb1a34a8b14594a1629c6c",
     "url": "/Server-In-Client/index.html"
   },
   {
-    "revision": "f906c80331fa0f0524bb",
+    "revision": "9571ed3401faa128a901",
     "url": "/Server-In-Client/static/css/main.3ce3b882.chunk.css"
   },
   {
-    "revision": "4da1c1b4ce955061d276",
-    "url": "/Server-In-Client/static/js/2.82d744f7.chunk.js"
+    "revision": "c27c53250f53b0de97cb",
+    "url": "/Server-In-Client/static/js/2.e19a6575.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/Server-In-Client/static/js/2.82d744f7.chunk.js.LICENSE.txt"
+    "url": "/Server-In-Client/static/js/2.e19a6575.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f906c80331fa0f0524bb",
-    "url": "/Server-In-Client/static/js/main.bdd59d01.chunk.js"
+    "revision": "9571ed3401faa128a901",
+    "url": "/Server-In-Client/static/js/main.f05321de.chunk.js"
   },
   {
     "revision": "9e0545a171547431bfe9",
